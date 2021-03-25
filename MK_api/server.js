b5cebef6ef31e3174d12e46142eb329312e2dd5e
Gil Solis <gilsolisuw@gmail.com>
2021-03-25 12:22:51 -0700

    finished get

--- MK_api/server.js
+++ MK_api/server.js
@@ -6,16 +6,43 @@ const PORT = 8000
 app.use(cors())
 
 let characters = {
-    name: "Goro",
-    fatality: "Dragon Fangs"
-
+    'goro': {
+        moveset: "Fireball and Goro Grab",
+        fatality: "Dragon Fangs"
+    },
+    'johnny cage': {
+        moveset: "Green Shadow Kick and Straight Forceball",
+        fatality: "Triple Punch Decapitation"
+    },
+    'sub-zero': {
+        moveset: "Ice Blast and Ice Shower",
+        fatality: "Snowball Grenade"
+    },
+    'scorpion': {
+        moveset: "Demon Fire and Spine Rip",
+        fatality: "Dragon Fangs"
+    },
+    'sonya blade': {
+        moveset: "Armed Forces andInverted Bicycle Kick",
+        fatality: "Fire Kiss"
+    },
+    'unknown': {
+        moveset: "unknown",
+        fatality: "unknown"
+    }
 }
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
-app.get('/api/character', (request, response) => {
-    response.json(characters)
+app.get('/api/characters/:characterName', (request, response) => {
+    const name = request.params.characterName.toLowerCase()
+    console.log(name)
+    if (characters[name]) {
+        response.json(characters[name])
+    } else {
+        response.json(characters['unknown'])
+    }
 })
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
--- client-side-MK/js/main.js
+++ client-side-MK/js/main.js
@@ -2,10 +2,12 @@
 document.querySelector('button').addEventListener('click', getCharacter)
 
 async function getCharacter() {
+    const name = document.querySelector('input').value
     try {
-        const res = await fetch('http://localhost:8000/api/character')
+        const res = await fetch(`http://localhost:8000/api/characters/${name}`)
         const data = await res.json()
         console.log(data)
+        document.querySelector('h2').innerText = data.fatality
     } catch (err) {
         console.log(err)
 
